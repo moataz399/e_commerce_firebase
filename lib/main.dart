3e1edf4fec30568f4e42086147a644c5223b36bd
moataz399 <moatazzezo41@gmail.com>
2024-02-17 16:22:38 +0200

    - using cli to generate firebase options file - ready to code now

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:e_commerce_firebase/firebase_options.dart';
 import 'package:e_commerce_firebase/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'core/di/dependency_injection.dart';
 import 'core/helpers/bloc_observer.dart';
 import 'core/routing/app_router.dart';
 
-void main() {
+
+Future<void> main() async {
   setUpGetIt();
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp(appRouter: AppRouter()));
 }
