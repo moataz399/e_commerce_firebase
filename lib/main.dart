d6c36601d312a568b72ab460eba0de30fac04790
AbdoAlhady <abdo.agiry90@gmail.com>
2024-02-17 22:20:44 +0200

    add splash screen to ios &android below 12

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   // Abdo Al hady
   runApp(MyApp(appRouter: AppRouter()));
 }
+//dart run flutter_native_splash:create --path=flutter_native_splash.yaml
