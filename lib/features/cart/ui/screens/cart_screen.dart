0bb9e4b81ea38c2e3fb8c24674e0665d010b4db0
AbdoAlhady <abdo.agiry90@gmail.com>
2024-03-08 18:24:10 +0200

    refactor cart checkout

--- lib/features/cart/ui/screens/cart_screen.dart
+++ lib/features/cart/ui/screens/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:e_commerce_firebase/core/theming/colors.dart';
 import 'package:e_commerce_firebase/core/theming/text_styles.dart';
 import 'package:e_commerce_firebase/core/utils/app_strings.dart';
 import 'package:e_commerce_firebase/core/widgets/app_text_button.dart';
+import 'package:e_commerce_firebase/features/cart/ui/widgets/cart_checkout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../widgets/cart_coupon_and_order_summry.dart';
@@ -32,38 +33,8 @@ class CartScreen extends StatelessWidget {
           ),
         ),
       ),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
-        height: 140.h,
-        decoration: const BoxDecoration(
-          color: AppColors.white,
-          boxShadow: [
-            BoxShadow(
-              color: AppColors.lighterGray,
-              blurRadius: 10,
-              spreadRadius: 1.0,
-              offset: Offset(0.0, 5.0),
-            )
-          ],
-        ),
-        child:  Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('2 Items',style: TextStyles.font12GrayRegular),
-                 Text('\$ 20', style: TextStyles.font14GreenBold),
-              ],
-            ),
-            verticalSpace(12),
-            AppTextButton(buttonText: AppStrings.checkout, textStyle: TextStyles.font15WhiteBold, onPressed: () {
-              
-            },
-            buttonHeight: 56,)
-
-          ],
-        ),
-      ),
+      bottomNavigationBar: const CartCheckout(),
     );
   }
 }
+
