1836e9670d7e3aa343a1565ec85cadec79ce911c
AbdoAlhady <abdo.agiry90@gmail.com>
2024-03-08 05:19:51 +0200

    added app icons

---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const _kFontFam = 'AppIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData coupon = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
